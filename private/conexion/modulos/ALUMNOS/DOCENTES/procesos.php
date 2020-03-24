9e128883ec03ffb704fc64417c7d146dacbfa192
unknown <jimenezcruz848@gmail.com>
2020-03-23 18:24:38 -0600

    form alumno terminado

--- private/conexion/modulos/ALUMNOS/DOCENTES/procesos.php
+++ private/conexion/modulos/ALUMNOS/DOCENTES/procesos.php
@@ -1,23 +1,23 @@
 <?php 
 include('../../Config/Config.php');
-$docentes = new docentes($conexion);
+$alumno = new docente($conexion);
 
 $proceso = '';
 if( isset($_GET['proceso']) && strlen($_GET['proceso'])>0 ){
 	$proceso = $_GET['proceso'];
 }
-$docentes->$proceso( $_GET['docentes'] );
-print_r(json_encode($docentes->respuesta));
+$alumno->$proceso( $_GET['docente'] );
+print_r(json_encode($docente->respuesta));
 
-class docentes{
+class docente{
     private $datos = array(), $db;
     public $respuesta = ['msg'=>'correcto'];
     
     public function __construct($db){
         $this->db=$db;
     }
-    public function recibirDatos($docentes){
-        $this->datos = json_decode($docentes, true);
+    public function recibirDatos($docente){
+        $this->datos = json_decode($docente, true);
         $this->validar_datos();
     }
     private function validar_datos(){
@@ -30,22 +30,53 @@ class docentes{
         if( empty($this->datos['direccion']) ){
             $this->respuesta['msg'] = 'por favor ingrese la direccion del docente';
         }
+        if( empty($this->datos['dui']) ){
+            $this->respuesta['msg'] = 'por favor ingrese el dui del docente';
+        }
         $this->almacenar_docente();
     }
     private function almacenar_docente(){
         if( $this->respuesta['msg']==='correcto' ){
             if( $this->datos['accion']==='nuevo' ){
                 $this->db->consultas('
-                    INSERT INTO docentes (codigo,nombre,direccion,telefono) VALUES(
+                    INSERT INTO docente (codigo,nombre,dui,direccion,telefono) VALUES(
                         "'. $this->datos['codigo'] .'",
                         "'. $this->datos['nombre'] .'",
+                        "'. $this->datos['dui'] .'",
                         "'. $this->datos['direccion'] .'",
                         "'. $this->datos['telefono'] .'"
                     )
                 ');
                 $this->respuesta['msg'] = 'Registro insertado correctamente';
+            } else if( $this->datos['accion']==='modificar' ){
+                $this->db->consultas('
+                   UPDATE docente SET
+                        codigo     = "'. $this->datos['codigo'] .'",
+                        nombre     = "'. $this->datos['nombre'] .'",
+                        direccion  = "'. $this->datos['direccion'] .'",
+                        dui        = "'. $this->datos['dui'] .'",
+                        telefono   = "'. $this->datos['telefono'] .'"
+                    WHERE idDocente = "'. $this->datos['idDocente'] .'"
+                ');
+                $this->respuesta['msg'] = 'Registro actualizado correctamente';
             }
         }
+    }
+    public function buscarDocente($valor=''){
+        $this->db->consultas('
+            select docentes.idDocente, docentes.codigo, docentes.nombre, docentes.dui, docentes.direccion, docentes.telefono
+            from docentes
+            where docentes.codigo like "%'.$valor.'%" or docentes.nombre like "%'.$valor.'%"
+        ');
+        return $this->respuesta = $this->db->obtener_datos();
+    }
+    public function eliminarDocente($idDocente=''){
+        $this->db->consultas('
+            delete docentes
+            from docentes
+            where docentes.idDocente = "'.$idDocente.'"
+        ');
+        $this->respuesta['msg'] = 'Registro eliminado correctamente';
     }
 }
 ?>
--- public/vistas/Alumnos/Docentes/docentes.js
+++ public/vistas/Alumnos/Docentes/docentes.js
@@ -1,21 +1,29 @@
-var $ = el => document.querySelector(el),
-    frmAlumnos = $("#frmDocentes");
-frmAlumnos.addEventListener("submit",e=>{
-    e.preventDefault();
-    e.stopPropagation();
-    
-    let docentes = {
-        accion    : 'nuevo',
-        codigo    : $("#txtCodigoDocentes").value,
-        nombre    : $("#txtNombreDocentes").value,
-        direccion : $("#txtDireccionDocentes").value,
-        telefono  : $("#txtTelefonoDocentes").value
-    };
-    fetch(`private/Modulos/docentes/procesos.php?proceso=recibirDatos&docentes=${JSON.stringify(docentes)}`).then( resp=>resp.json() ).then(resp=>{
-        $("#respuestaDocentes").innerHTML = `
-            <div class="alert alert-success" role="alert">
-                ${resp.msg}
-            </div>
-        `;
+export function modulo(){
+    var $ = el => document.querySelector(el),
+        frmAlumnos = $("#frm-docentes");
+    frmAlumnos.addEventListener("submit",e=>{
+        e.preventDefault();
+        e.stopPropagation();
+        
+        let alumnos = {
+            accion    : frmAlumnos.dataset.accion,
+            idAlumno  : frmAlumnos.dataset.iddocente,
+            codigo    : $("#txtCodigoDocente").value,
+            nombre    : $("#txtNombreDocente").value,
+            dui       : $("#txtDuiDocente").value,
+            direccion : $("#txtDireccionDocente").value,
+            telefono  : $("#txtTelefonoDocente").value
+        };
+        fetch(`private/Modulos/docentes/procesos.php?proceso=recibirDatos&docente=${JSON.stringify(docentes)}`).then( resp=>resp.json() ).then(resp=>{
+            $("#respuestaDocente").innerHTML = `
+                <div class="alert alert-success" role="alert">
+                    ${resp.msg}
+                </div>
+            `;
+        });
     });
-});
+    frmAlumnos.addEventListener("reset",e=>{
+        $("#frm-docentes").dataset.accion = 'nuevo';
+        $("#frm-docentes").dataset.iddocente = '';
+    });
+}
